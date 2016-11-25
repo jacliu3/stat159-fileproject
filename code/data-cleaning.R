5d026552a5e9eaae68d60ea6264b81f9dd1ae8a6
zhongling <jzljohn18@berkeley.edu>
2016-11-24 19:44:22 -0800

    code: add data-cleaning

---
+++ code/data-cleaning.R
@@ -0,0 +1,17 @@
+#Part of our data cleaning process involves dealing with NA values. There are three types
+#of NA value in this dataset: NULL and 'PrivacySuppressed' and actual NA's. So I suggest when we are
+#loading dataset, we could add 'na.string ="PrivacySuppressed', turning 
+#PrivacySuppressed into NA's. And then turn NULL values into 0.
+
+# We claim here that for those features that contain over 10 (or 20) percent of NA values,
+# they should be discarded. 
+
+#Example: Repayment Data
+
+#define functions that calculate the proportion of NA's in a feature
+pMiss <- function(x){sum(is.na(x))/length(x)*100} 
+repayment <- read.csv(file = '../data/repayment.csv', header = TRUE, na.strings = "PrivacySuppressed", stringsAsFactors = FALSE)
+repayment[which(repayment== "NULL", arr.ind=TRUE)] <- 0
+vars.na <- apply(repayment,2,pMiss)
+repayment.clean <- repayment[,-which(vars.na > 25)] # if we set 25 percent NA rate as boundary
+
